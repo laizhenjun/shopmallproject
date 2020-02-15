8d27f3c48d2badbc0aae9049f5b41a395a6837e9
Ryan.lai <lai@pingdandeMacBook-Pro.local>
2020-02-15 19:36:35 +0800

    首页部分功能的补充；Add home function

---
+++ src/views/home/childcomponents/homeRecommendView.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="recommend">
+	<div v-for="item in recommends" class="recommend-item">
+		<a :href="item.link">
+			<img :src="item.image"/>
+			<div>{{item.title}}</div>
+		</a>
+	</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'homeRecommendView',
+		props:{
+			recommends:{
+				type:Array,
+				default(){
+					return []
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	.recommend{
+		display: flex;
+		width: 100%;
+		text-align: center;
+		font-size: 0.75rem;
+		padding: 0.9375rem 0rem 1.25rem;
+		border-bottom: 0.5rem solid #eee;
+	}
+	.recommend-item{
+		flex: 1;
+	}
+	.recommend-item img{
+		width: 4.0625rem;
+		height: 4.0625rem;
+		margin-bottom: 0.625rem;
+	}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,9 +1,56 @@
 <template>
-	<div><h2>我的首页</h2></div>
+	<div id="home">
+		<nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+		<home-swiper :banners="banners"></home-swiper>
+		<Recommend-view :recommends="recommends"></Recommend-view>
+		<feature-view></feature-view>
+	</div>
 </template>
 
 <script>
+	import NavBar from '../../components/common/navBar/navBar.vue'
+	// import Swiper from '../../components/common/swiper/Swiper.vue'
+	// import SwiperItem from '../../components/common/swiper/SwiperItem.vue'
+	import HomeSwiper from './childcomponents/homeSwiper.vue'
+	import RecommendView from './childcomponents/homeRecommendView.vue'
+	import FeatureView from './childcomponents/featureView.vue'
+	import {getHomeMultidata} from '../../network/home.js'
+	export default{
+		name:'home',
+		components:{
+			NavBar,
+			HomeSwiper,
+			RecommendView,
+			FeatureView
+		},
+		data(){
+			return{
+				banners:[],
+				recommends:[]
+			}
+		},
+		created() {
+			getHomeMultidata().then(res => {
+				// this.banners = res.data.data.banner.list;
+				// this.recommends = res.data.data.recommend.list;
+				 this.banners = res.data.data.banner.list;
+				 this.recommends = res.data.data.recommend.list;
+			})
+		}
+	}
 </script>
 
 <style>
+	#home{
+		padding-top: 2.75rem;
+	}
+	.home-nav{
+		background-color: hotpink;
+		color: #fff;
+		position: fixed;
+		left: 0;
+		right: 0;
+		top: 0;
+		z-index: 9;
+	}
 </style>
